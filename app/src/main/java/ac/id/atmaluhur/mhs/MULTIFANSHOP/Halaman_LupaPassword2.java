4fe8deeb3931669b6e65b00565fddbc15df30f74
yunita278 <yunita278yani@gmail.com>
2022-01-25 11:35:52 +0700

    Pratiwi
    Lupa password 1
    Lupa password 2

--- app/src/main/java/ac/id/atmaluhur/mhs/MULTIFANSHOP/Halaman_LupaPassword2.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/MULTIFANSHOP/Halaman_LupaPassword2.java
@@ -1,13 +1,11 @@
 package ac.id.atmaluhur.mhs.MULTIFANSHOP;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Halaman_LupaPassword2 extends AppCompatActivity {
-
+public class Halaman_LupaPassword2 extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
